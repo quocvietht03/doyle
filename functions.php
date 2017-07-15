a62cac9e5928a0724dff08d273ac577bdc6d145d
QuocViet <quocvietht03@gmail.com>
2017-07-15 11:56:39 +0700

    update file functions.php

--- functions.php
+++ functions.php
@@ -23,4 +23,3 @@ if (class_exists('Woocommerce')) {
     require_once get_template_directory() . '/woocommerce/wc-template-functions.php';
     require_once get_template_directory() . '/woocommerce/wc-template-hooks.php';
 }
-die('hi!');
